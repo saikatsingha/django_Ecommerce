c66ec0014759b7b4508e6615f77d05afe36d076b
saikatsingha <saikatsingha@gmail.com>
2022-09-01 21:46:41 +0530

    postgresql setup done

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -73,11 +73,22 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'ecommerce',
+       'USER': 'saikat',
+       'PASSWORD': '1234',
+       'HOST': 'localhost',
+       'PORT': '',
+   }
 }
 
 
